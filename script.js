314deb1262168665706ac66dba253b310332d2ee
Juan Villegas <jvillegas422@gmail.com>
2022-05-15 07:53:52 -0500

    Complete overhaul of html, css, & js

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
-$(document).ready();
+console.log('In script.js');
 
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('Ready!');
+    $(document).on('click', '#submit-btn', onSubmit);
+}
+
+function onSubmit() {
+    let firstName = $('#first-name').val();
+    let lastName = $('#last-name').val();
+    let idNumber = $('#id-number').val();
+    let jobTitle = $('#job-title').val();
+    let salary = $('#annual-salary').val();
+
+    $('#employee-table').append(`
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${idNumber}</td>
+            <td>${jobTitle}</td>
+            <td>${salary}</td>
+            <td>
+                <button class="delete-btn">
+                    Delete
+                </button>
+            </td>
+        </tr>
+    `);
+
+    $('#first-name').val('');
+    $('#last-name').val('');
+    $('#id-number').val('');
+    $('#job-title').val('');
+    $('#annual-salary').val('');
+}
